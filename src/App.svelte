bf6682d48188678b827aa6c234de43fafc44122f
Adedayo <falola.adedayo@fairmoney.io>
2023-11-27 12:56:12 +0100

    svelte checkout v1 push

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import CheckoutScript from './CheckoutScript.svelte';
+</script>
+
+<CheckoutScript
+  public_key="SBTESTPUBK_t4G16GCA1O51AV0Va3PPretaisXubSw1"
+  tranref={new Date().getTime()}
+  currency="NGN"
+  country="NG"
+  amount="145.00"
+  email="test@emaildomain.com"
+  mobile_no=""
+  productId=""
+  description=""
+  setAmountByCustomer={false}
+  full_name="John Doe"
+  tokenize={false}
+  planId=""
+  pocketReference=""
+  vendorId=""
+  callbackurl="http://yourdomain.com"
+  buttonText="Make Payment"
+  buttonCustomization={{
+    paddingVertical: 40,
+    paddingHorizontal: 40,
+    width: 50,
+    color: "#fff",
+    backgroundColor: "#000",
+    fontSize: 10,
+    borderRadius:20
+  }}
+/>
+
+
---
+++ src/CheckoutScript.svelte
@@ -0,0 +1,72 @@
+<script>
+  export let public_key;
+  export let tranref;
+  export let currency;
+  export let country;
+  export let amount;
+  export let email;
+  export let mobile_no;
+  export let productId;
+  export let description;
+  export let setAmountByCustomer;
+  export let full_name;
+  export let tokenize;
+  export let planId;
+  export let pocketReference;
+  export let vendorId;
+  export let callbackurl;
+  export let buttonText;
+  export let buttonCustomization;
+
+
+  function paywithSeerbit() {
+    SeerbitPay({
+      public_key,
+      tranref,
+      currency,
+      country,
+      amount,
+      email,
+      mobile_no,
+      productId,
+      description,
+      setAmountByCustomer,
+      full_name,
+      tokenize,
+      planId,
+      pocketReference,
+      vendorId,
+      callbackurl,
+    },
+    function callback(response, closeModal) {
+      console.log(response);
+    },
+    function close(close) {
+      console.log(close);
+    });
+  }
+</script>
+
+<svelte:head>
+  <title>SeerBit Simple Checkout</title>
+  <script src="https://checkout.seerbitapi.com/api/v2/seerbit.js"></script>
+</svelte:head>
+
+<body>
+  <button 
+  style="
+      padding-top:{buttonCustomization.paddingVertical}px;
+      padding-bottom: {buttonCustomization.paddingVertical}px;
+      padding-left: {buttonCustomization.paddingHorizontal}px;
+      padding-right: {buttonCustomization.paddingHorizontal}px;
+      font-size: {buttonCustomization.fontSize}px;
+      background-color: {buttonCustomization.backgroundColor};
+      color:{buttonCustomization.color};
+      width: {buttonCustomization.width}%;
+      border-radius: {buttonCustomization.borderRadius}px;
+      border: none;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+  "
+  on:click={paywithSeerbit}>{buttonText}</button>
+</body>
